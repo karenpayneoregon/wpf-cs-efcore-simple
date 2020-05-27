5916160738366ae35299ef6694c2164b10d2138c
Karen Payne <paynekaren@comcast.net>
2020-05-27 10:28:37 -0700

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using Microsoft.EntityFrameworkCore;
+using WpfApp1.Contexts;
+using WpfApp1.Models;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        bool _hasShown;
+
+        protected override void OnContentRendered(EventArgs e)
+        {
+            base.OnContentRendered(e);
+
+            if (_hasShown)
+            {
+                return;
+            }
+
+            _hasShown = true;
+
+            using (var context = new HRContext())
+            {
+                var employeeCollection = new ObservableCollection<Employees>(context.Employees.ToList());
+                EmployeeGrid.ItemsSource = employeeCollection;
+            }
+
+        }
+        private void ChangeText(object sender, RoutedEventArgs e)
+        {
+            var currentEmployee = (Employees) (sender as Button)?.DataContext;
+            MessageBox.Show(
+                $"{currentEmployee.EmployeeId}: " + 
+                           $"{currentEmployee.FirstName} " + 
+                           $"{currentEmployee.LastName}", "Current Employee", 
+                MessageBoxButton.OK, 
+                MessageBoxImage.Information);
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# About
+
+Simple example for loading a DataGrid from Entity Framework Core 3x with buttons to obtain the current model instance, in this case an employee from a SQL-Server database table.
