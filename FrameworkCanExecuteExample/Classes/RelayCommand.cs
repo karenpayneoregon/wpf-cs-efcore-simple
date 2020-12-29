9115c0fe6af94890793ef722a0543f7bd755cc7b
Karen Payne <Karen.1.payne@oregon.gov>
2020-12-29 04:52:08 -0800

    refactor C# 9

--- FrameworkCanExecuteExample/Classes/RelayCommand.cs
+++ FrameworkCanExecuteExample/Classes/RelayCommand.cs
@@ -22,7 +22,7 @@ namespace FrameworkCanExecuteExample.Classes
 
         public bool CanExecute(object parameter)
         {
-            return _canExecute == null || _canExecute(parameter);
+            return _canExecute is null || _canExecute(parameter);
         }
 
         public void Execute(object parameter)
